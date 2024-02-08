edc9f9033bf672ad93a85693b27088ddc541ebb4
ibra-kdbra <git-repo-develop@outlook.com>
2024-02-08 20:53:58 +0300

    fixed

---
+++ src/PropertyProcessor/Decorator/TypeHint/ArrayTypeHintDecorator.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPModelGenerator\PropertyProcessor\Decorator\TypeHint;
+
+use PHPModelGenerator\Model\Property\PropertyInterface;
+
+/**
+ * Class ArrayTypeHintDecorator
+ *
+ * @package PHPModelGenerator\PropertyProcessor\Decorator\Property
+ */
+class ArrayTypeHintDecorator implements TypeHintDecoratorInterface
+{
+    /** @var PropertyInterface */
+    protected $nestedProperty;
+
+    private $recursiveArrayCheck = 0;
+
+    /**
+     * ArrayTypeHintDecorator constructor.
+     *
+     * @param PropertyInterface $nestedProperty
+     */
+    public function __construct(PropertyInterface $nestedProperty)
+    {
+        $this->nestedProperty = $nestedProperty;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function decorate(string $input, bool $outputType = false): string
+    {
+        if (++$this->recursiveArrayCheck > 1) {
+            return $this->nestedProperty->getTypeHint($outputType, [self::class]);
+        }
+
+        $result = implode(
+            '|',
+            array_map(
+                static function (string $typeHint): string {
+                    return "{$typeHint}[]";
+                },
+                explode('|', $this->nestedProperty->getTypeHint($outputType))
+            )
+        );
+
+        $this->recursiveArrayCheck--;
+
+        return $result;
+    }
+}

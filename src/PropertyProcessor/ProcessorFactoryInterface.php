1ab823b92bf510aa5b68cfa1343f260ce56b75c0
ibra-kdbra <git-repo-develop@outlook.com>
2024-02-19 23:45:04 +0300

    interface

---
+++ src/PropertyProcessor/ProcessorFactoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPModelGenerator\PropertyProcessor;
+
+use PHPModelGenerator\Model\Schema;
+use PHPModelGenerator\SchemaProcessor\SchemaProcessor;
+
+/**
+ * Interface ProcessorFactoryInterface
+ *
+ * @package PHPModelGenerator\PropertyProcessor
+ */
+interface ProcessorFactoryInterface
+{
+    /**
+     * @param string|array               $type
+     * @param PropertyMetaDataCollection $propertyMetaDataCollection
+     * @param SchemaProcessor            $schemaProcessor
+     * @param Schema                     $schema
+     *
+     * @return PropertyProcessorInterface
+     */
+    public function getProcessor(
+        $type,
+        PropertyMetaDataCollection $propertyMetaDataCollection,
+        SchemaProcessor $schemaProcessor,
+        Schema $schema
+    ): PropertyProcessorInterface;
+}
